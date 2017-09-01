acf526ddceb8ed6a1317c71d0ee0bdd724afc396
whiteaj2 <whiteaj2@rose-hulman.edu>
2017-08-31 23:48:14 -0400

    Fixed all errors, got program to exit 0

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -2,11 +2,11 @@
 This module lets you practice correcting SYNTAX (notation) errors.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Andrew White.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   Locate the syntax (notation) errors in this file
 #   by looking for red underlines.
 #
@@ -32,20 +32,21 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 import rosegraphics as rg
 import math
 
+window = rg.TurtleWindow()
+
 
 def main():
     """ Calls the other functions in this module to demo them. """
     print_math()
-    tutle_fn()
+    turtle_fun()
 
 
-def print_math:
+def print_math():
     """ Prints some calculated values. """
-    x = cos(pi)
+    x = math.cos(math.pi)
     print(x)
-
-    y = sin(pi)
-    print(The sine of PI is, y)
+    y = math.sin(math.pi)
+    print('The sine of PI is', y)
 
 
 def turtle_fun():
@@ -54,22 +55,18 @@ def turtle_fun():
     constructs a classic SimpleTurtle and asks it to do some things,
     and waits for the user to click anywhere in the window to close it.
     """
-    window = rg.TurtleWindow()
-
     alan = rg.SimpleTurtle()
-alan.pen = rg.Pen('blue', 30)
+    alan.pen = rg.Pen('blue', 30)
     alan.paint_bucket = rg.PaintBucket('yellow')
-
-    alan.back(3 * (47 + 16)
-    al.begin_fill()
-    alan.circle(25)
+    alan.backward(3 * (47 + 16))
+    alan.begin_fill()
+    alan.draw_circle(25)
     alan.end_fill()
+    alan.forward(200)
 
-    forward(200)
-
-    window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
 main()
+window.close_on_mouse_click()
